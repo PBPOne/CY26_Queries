2ebad999fe426e55848632b3bd83808e97b720db
PBPOne <liyaquathusain@pbpartners.com>
2026-02-10 17:18:50 +0530

    Update New_Construct_2026_Health_Fresh_Query.sql

--- New_Construct_2026_Health_Fresh_Query.sql
+++ New_Construct_2026_Health_Fresh_Query.sql
@@ -153,6 +153,8 @@ from t4
 select
 PartnerCode,
 pd as product_name, MON,
+sum(TotalPremium) as TotalPremium,
+sum(APE) as APE, 
 sum(netpr) as Net_Pr,
 sum(Accrual_Net_Ins * special_deal_flag) as Accrual_Net_Booked,
 sum(Accrual_Net_Ins * policy_issued_flag * policy_verified_flag * special_deal_flag) as Accrual_Net,
@@ -161,3 +163,4 @@ from t5
 group by 
 PartnerCode,
 pd, MON
+
